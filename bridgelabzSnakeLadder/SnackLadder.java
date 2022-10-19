255ad4dc8a07dcf73abd0a978b0e37d9ecf378f4
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-20 01:48:47 +0530

    snack and ladder

---
+++ bridgelabzSnakeLadder/SnackLadder.java
@@ -0,0 +1,15 @@
+package com.bridgelabzSnakeLadder;
+
+public class SnackLadder {
+    public static final int POSITION = 0;
+    public static void main(String[] args) {
+        System.out.println("Snake & Ladder Simulator");
+        //Snake and Ladder game played with single player at start position 0
+        System.out.println("Let's begin the game ....");
+        System.out.println("Initial position is Square Zero");
+        System.out.println("position : Square "+POSITION);
+
+    }
+
+}
+
